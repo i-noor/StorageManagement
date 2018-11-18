1e8003d37a85beeb05ade80c71973bcfbecbab51
Ainur <ainur.muhametyanov@gmail.com>
2018-11-18 11:51:15 +0500

    last

--- StorageManagement/Controllers/HomeController.cs
+++ StorageManagement/Controllers/HomeController.cs
@@ -19,8 +19,10 @@ namespace StorageManagement.Controllers
         public string ajaxCheck;
         public ActionResult Index()
         {  
-            var transactions = db.Transactions.Include(p => p.Product); 
-            
+            var transactions = db.Transactions.Include(p => p.Product);
+            IEnumerable<Product> products = db.Products;
+            ViewBag.Products = products;
+            //if (ajaxCheck == "1") return PartialView("~/Views/Home/Transactions.cshtml");            
             return View(transactions.ToList());
         }       
         [HttpGet]
@@ -34,21 +36,10 @@ namespace StorageManagement.Controllers
             // получаем из бд все объекты Product
             IEnumerable<Product> products = db.Products;
             ViewBag.Products = products;
-            Product product = new Product();            
-            var productId = transaction.ProductId;
-            if (productId == null)
-            {
-                productsCount = db.Products.Count();
-                //Создание объекта для генерации чисел
-                do
-                {
-                    Random rnd = new Random();
-                    productId = rnd.Next(1, productsCount);
-                    product = db.Products.Find(productId);
-                } while (product == null);
-                transaction.ProductId = productId;
-            }            
+            Product product = new Product();
             transaction.Date = DateTime.Now;
+            var productId = transaction.ProductId;
+            product = db.Products.Find(productId);
             // передаем все объекты в динамическое свойство Products в ViewBag
             if (transaction.Operation == "Продажа")
             {
@@ -65,7 +56,7 @@ namespace StorageManagement.Controllers
             }
             else
             {
-                transaction.Cost = product.Price * 0.8 * transaction.Quantity;
+                transaction.Cost = product.Price * 0.5 * transaction.Quantity;
                 product.Quantity = product.Quantity + transaction.Quantity;
                 db.Entry(product).State = EntityState.Modified;
             }
@@ -78,6 +69,79 @@ namespace StorageManagement.Controllers
             if (Request.IsAjaxRequest())
                 ajaxCheck = "1";                
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Transactions(Transaction transaction)
+        {
+            ViewBag.LogText = "1";
+            // получаем из бд все объекты Product
+            IEnumerable<Product> products = db.Products;
+            ViewBag.Products = products;
+            Product product = new Product();
+            var productId = transaction.ProductId;
+            if (productId == null)
+            {
+                productsCount = db.Products.Count();
+                //Создание объекта для генерации чисел
+                do
+                {
+                    Random rnd = new Random();
+                    productId = rnd.Next(1, productsCount+2);
+                    product = db.Products.Find(productId);
+                } while (product == null);
+                transaction.ProductId = productId;
+            }
+            transaction.Date = DateTime.Now;
+            // передаем все объекты в динамическое свойство Products в ViewBag
+            if (transaction.Operation == "Продажа")
+            {
+                if (transaction.Quantity <= product.Quantity)
+                {
+                    transaction.Cost = product.Price * transaction.Quantity;
+                    product.Quantity = product.Quantity - transaction.Quantity;
+                    db.Entry(product).State = EntityState.Modified;
+                    // добавляем информацию о покупке в базу данных
+                    db.Transactions.Add(transaction);
+                    // сохраняем в бд все изменения
+                    db.SaveChanges();
+                }
+                else
+                {
+                    transaction.Operation = "Поступление";
+                    transaction.Quantity = 100;
+                    product.Quantity = product.Quantity + transaction.Quantity;
+                    transaction.Cost = product.Price * 0.5 * transaction.Quantity;
+                    db.Entry(product).State = EntityState.Modified;
+                    // добавляем информацию о покупке в базу данных
+                    db.Transactions.Add(transaction);
+                    // сохраняем в бд все изменения
+                    db.SaveChanges();
+                    ViewBag.LogText = "0";
+                }
+            }
+            else if (transaction.Operation == "Поступление")
+            {
+                transaction.Cost = product.Price * 0.5 * transaction.Quantity;
+                product.Quantity = product.Quantity + transaction.Quantity;
+                db.Entry(product).State = EntityState.Modified;
+                // добавляем информацию о покупке в базу данных
+                db.Transactions.Add(transaction);
+                // сохраняем в бд все изменения
+                db.SaveChanges();
+            } else
+            {
+                transaction.Cost = product.Price * 0.5 * transaction.Quantity;
+                product.Quantity = product.Quantity - transaction.Quantity;
+                db.Entry(product).State = EntityState.Modified;
+                // добавляем информацию о покупке в базу данных
+                db.Transactions.Add(transaction);
+                // сохраняем в бд все изменения
+                db.SaveChanges();
+            }            
+            //ajaxCheck = Request.Form["a"];
+            //if (true) return PartialView("~/Views/Home/Transactions");            
+            var transactions = db.Transactions.Include(p => p.Product);
+            return PartialView(transactions.ToList());
         }
     }
 }
--- StorageManagement/Scripts/script.js
+++ StorageManagement/Scripts/script.js
@@ -1,25 +1,81 @@
 ﻿$(document).ready(function () {    
-    var q = Math.round(getRandomArbitary(1, 500));    
+    var q = 1;
+    var trade = false;
     var op = "Продажа";
-    if (getRandomArbitary(0, 1)<0.5) op = "Продажа"
-    else op = "Поступление";   
+    var timerId;
     function getRandomArbitary(min, max) {
         return Math.random() * (max - min) + min;
     }
-
-    makeTransaction(q,op);
-    function makeTransaction(q,op) {        
-        $.ajax({            
+    if ($("#tradeButton").text() == "Остановить торговлю") {
+        timerId = setInterval(makeTransaction, 1000);
+        trade = true;
+    }
+    $("#tradeButton").click(function () {
+        if (trade == false) {
+            timerId = setInterval(makeTransaction, 1000);
+            trade = true;
+            $("#tradeButton").text("Остановить торговлю");
+        } else {
+            clearInterval(timerId);
+            trade = false;
+            $("#tradeButton").text("Начать торговлю");
+        }
+    });
+    $('#spButton').click(function () {
+        $('#sp').modal('hide');
+        $.ajax({
             type: 'POST',
-            url: '/Home/Buy',
+            url: '/Home/Transactions',
             data: {
-                a : 1,
-                Operation : op,                             
-                Quantity: q                
-            },            
+                a: 1,
+                Operation: op,
+                Quantity: q
+            },
             success: function (data) {
-                $("#transactions").html(data);                 
+                $("#transactions").html(data);
+                trade = true;
+                console.log(data);
             }
         });
+    });
+    function makeTransaction() {
+        if (trade) {
+            q = Math.round(getRandomArbitary(1, 10));
+            var rand = getRandomArbitary(0, 1);
+            if (rand < 0.8) {
+                op = "Продажа";
+            }
+            else if (rand < 0.9) {
+                op = "Поступление";
+                q = Math.round(getRandomArbitary(40, 50));
+            } else if (rand < 0.95){
+                trade = false;
+                op = "Списание";
+                q = Math.round(getRandomArbitary(10, 30));
+                $(".modal-body").text("Обнаружены просроченные продукты.");
+                $('#sp').modal('show');                
+                return 0;
+            } else {
+                trade = false;
+                op = "Списание";
+                q = Math.round(getRandomArbitary(10, 30));
+                $(".modal-body").text("Пропажа продуктов");
+                $('#sp').modal('show');
+                return 0;
+            }
+            $.ajax({
+                type: 'POST',
+                url: '/Home/Transactions',
+                data: {
+                    a: 1,
+                    Operation: op,
+                    Quantity: q
+                },
+                success: function (data) {
+                    $("#transactions").html(data);
+                    console.log(data);
+                }
+            });
+        }
     }
 })
